10fb3bedb1ebec192823b999ba4ae2706c867577
tefy1908 <tefymaherison19@gmail.com>
2024-05-15 16:01:45 +0200

    task config models ressources

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_restful import Api
+from models import db
+import config
+from ressources import TaskList
 
+# Create the Flask application and the Flask-RESTful API manager.
 app = Flask(__name__)
+app.config.from_object(config)
+# Initialize the Flask-SQLAlchemy object.
+db.init_app(app)
+# Create the Flask-RESTful API manager.
+api = Api(app)
+# Create the endpoints.
+api.add_resource(TaskList, '/tasks')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+if __name__ == '__main__':
+    # Create the database tables.
+    with app.app_context():
+        db.create_all()
+    # Start the Flask development web server.
+    app.run(debug=True)
